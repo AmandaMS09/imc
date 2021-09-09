ddc8cf26ad15f36984bc22ef612b1310191aa8c3
Amanda Moura de Souza <78311166+AmandaMS09@users.noreply.github.com>
2021-09-09 00:54:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 //window.alert('Olá mundo')
 /*window.confirm('Você é legal?')*/
-window.prompt('Qual é o seu nome?')
+let nome = window.prompt('Qual é o seu nome?')
+let peso, altura, imc, result
+let aviso = document.getElementById("resultado")
+
+function calcular() {
+    peso = document.getElementById("peso").textContent;
+    altura = document.getElementById("altura").textContent;
+
+    imc = peso / (altura * altura)
+    if(imc <= 18.5) {
+        result = "abaixo do peso ideal"
+    }
+    else if(imc <= 24.9) {
+        result = "com o peso ideal"
+    }
+    else if(imc <= 29.9) {
+        result = "com um sobrepeso"
+    }
+    else if(imc <= 34.9) {
+        result = "com obesidade I"
+    }
+    else if(imc <= 39.9) {
+        result = "com obesidade II"
+    }
+    else {
+        result = "com obesidade III"
+    }
+
+    aviso.innerHTML += "\n Olá, " + nome + "! Com o IMC: "  + imc + "Você está " + result;
+}
