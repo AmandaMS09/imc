f64e8493b2de6a0880a594373768f0ad7b17e702
Amanda Moura de Souza <78311166+AmandaMS09@users.noreply.github.com>
2021-09-13 08:51:28 -0300

    Update de inserção da interação(script.js)
    Criação da função de cálculo do IMC, chamada através de um event listener.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
-//window.alert('Olá mundo')
-/*window.confirm('Você é legal?')*/
-let nome = window.prompt('Qual é o seu nome?')
-let peso, altura, imc, result
-let aviso = document.getElementById("resultado")
+//Pegando o evento de clique do botão calcular
+const calcular = document.getElementById('calcular');
+calcular.addEventListener('click', imc);
 
-function calcular() {
-    peso = document.getElementById("peso").value;
-    altura = document.getElementById("altura").value;
+//Criação da função que realizará o cálculo
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
 
-    imc = peso / (altura * altura)
-    if(imc <= 18.5) {
-        result = "abaixo do peso ideal"
-    }
-    else if(imc <= 24.9) {
-        result = "com o peso ideal"
-    }
-    else if(imc <= 29.9) {
-        result = "com um sobrepeso"
-    }
-    else if(imc <= 34.9) {
-        result = "com obesidade I"
-    }
-    else if(imc <= 39.9) {
-        result = "com obesidade II"
-    }
-    else {
-        result = "com obesidade III"
+    //Validação da entrada de dados
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso/(altura**2)).toFixed(1);
+        
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com o peso ideal. Parabéns!';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II.';
+        }else {
+            classificacao = 'com obesidade grau III.';
+        }
+
+        //Imprimir o resultado visto que foi tudo inserido corretamente
+        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}`;
+
+    }else {
+        //Imprimir aviso caso os dados não tenham sido inseridos corretamente
+        resultado.textContent = 'Para calcular o seu IMC , preencha todos os campos.';
     }
 
-    aviso.innerHTML += "\n Olá, " + nome + "! Com o IMC: "  + imc + "Você está " + result;
 }
